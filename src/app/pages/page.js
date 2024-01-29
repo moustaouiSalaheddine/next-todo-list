ed4191f3ed87c66b729f286dadfbd878962f0dd4
moustaouiSalaheddine <moustaoui.salaheddine@gmail.com>
2024-01-29 21:11:23 +0100

    example mui components

--- src/app/pages/page.js
+++ src/app/pages/page.js
@@ -1,7 +1,22 @@
 import React from "react";
+import ButtonUsage from "../../../components/ButtonUsage";
+import Image from "next/image";
+import { Box } from "@mui/system";
+import { Paper } from "@mui/material";
 
 function page() {
-  return <div>this is static page</div>;
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column" }}>
+      <p>this is static page</p>
+      <Image
+        src={"https://via.placeholder.com/250x150/"}
+        width={250}
+        height={250}
+      />
+      <ButtonUsage />
+      <Paper sx={{}}></Paper>
+    </Box>
+  );
 }
 
 export default page;
