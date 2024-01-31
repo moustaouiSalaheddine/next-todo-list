33207b2fc993fe22f7f68dcceec9b7507af9b0f0
moustaouiSalaheddine <moustaoui.salaheddine@gmail.com>
2024-01-31 17:11:05 +0100

    add metadata in product details

--- src/app/products/[productId]/page.js
+++ src/app/products/[productId]/page.js
@@ -1,6 +1,21 @@
 import CustomizedTabHeaders from "@/components/CustomizedTabHeaders";
 import { Box, Typography } from "@mui/material";
 
+export const generateMetadata = async ({ params }) => {
+  const { productId } = params;
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(productId);
+    }, 1000);
+  });
+  const metadata = {
+    title: `Product ${title}`,
+    description: `This is the description for Product ${title}`,
+    author: "Moustaoui Salaheddine",
+    date: new Date().toISOString(),
+  };
+  return metadata;
+};
 const ProductDetails = ({ params }) => {
   return (
     <>
