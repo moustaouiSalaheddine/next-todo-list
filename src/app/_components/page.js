c456f4d9529b894aa807c0d727fa0610f3b209c9
moustaouiSalaheddine <moustaoui.salaheddine@gmail.com>
2024-02-01 15:30:08 +0100

    Private Folders : Opt folder and all child segments out of routing

---
+++ src/app/_components/page.js
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+
+const PrivateRoute = () => {
+  return (
+    <>
+      <Typography variant="h1" style={{}}>
+        You cannot view this in the browser
+      </Typography>
+    </>
+  );
+};
+export default PrivateRoute;
